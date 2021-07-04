b7d9ac884ca8750dcbd68cd184809e66eb906bfe
SaurishD <saurishdarodkar@gmail.com>
2021-07-04 17:04:12 +0530

    Initial Commit. Make Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MachineLearningVisualizer';
+  dots:any  = [];
+  line = [0,310,2000,0]
+  makeDot(event: any){
+    console.log("clicked")
+    //this.line[0] = event.offsetX;
+    //this.line[1] = event.offsetY;
+    const temp = [event.offsetX,event.offsetY];
+    this.dots.push(temp);
+
+  }
 }

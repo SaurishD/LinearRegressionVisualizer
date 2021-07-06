4b3dfb481758e8ed0605f01fba1f7ccb09aae081
SaurishD <saurishdarodkar@gmail.com>
2021-07-06 09:20:45 +0530

    Implement basic linear regression

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MachineLearningVisualizer';
+  title = 'Linear Regression Visualizer';
   dots:any  = [];
-  line = [0,310,2000,0]
+  line = [0,0,2000,0];
+  editMode: Boolean = true;
+  learningRate = 0.01;
+  iterations = 10;
+  x:any = [];
+  y:any = [];
+  t0 = 0;
+  t1 = 0;
+  cost: number = 0;
   makeDot(event: any){
-    console.log("clicked")
-    //this.line[0] = event.offsetX;
-    //this.line[1] = event.offsetY;
+    if(!this.editMode) return;
     const temp = [event.offsetX,event.offsetY];
     this.dots.push(temp);
+  }
 
+  async GradientDescent(){
+    this.editMode = false;
+    var m: number = this.dots.length;
+    this.x = new Array(m);
+    this.y = new Array(m);
+    for (let i = 0; i < m; i++) {
+      this.x[i] = this.dots[i][0]/100 ;
+      this.y[i] = this.dots[i][1]/100;
+    }
+    var h:number[] = new Array(m);
+
+    for (let itr = 0; itr < this.iterations; itr++) {
+      for (let i = 0; i < m; i++) {
+        h[i]=this.t0 + this.t1*this.x[i];    
+      }
+
+      
+
+      this.cost = 0;
+      for(let i=0;i<m;i++){
+        this.cost += (h[i]-this.y[i])*(h[i]-this.y[i]);
+      }
+      this.cost = this.cost/(2*m);
+
+      var temp: number = 0;
+      for(let i=0;i<m;i++){
+        temp += (h[i]-this.y[i]);
+      }
+      this.t0 = this.t0 - (this.learningRate*temp)/m;
+
+      temp = 0;
+      for(let i=0;i<m;i++){
+        temp += (h[i]-this.y[i])*this.x[i];
+      }
+      console.log(temp);
+      this.t1 = this.t1 - (this.learningRate*temp)/m;
+
+      this.line[1] = this.t0;
+      this.line[3] = this.t0 + 2000*this.t1;
+
+      await new Promise(f => setTimeout(f,1000));
+
+      
+    }
+
+
+    this.editMode = true;
   }
 }
